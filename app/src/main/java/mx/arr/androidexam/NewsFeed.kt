5511c9e3bbf987e591e9a804280cf53fb257e680
Andres R <A01746592@itesm.mx>
2021-05-05 01:33:37 -0500

    Boton de Login cambia a la pantalla principal, falta que pregunte la ubicación. Usa shared preferences para simular el login. Se añadio el botón de logout de igual manera ya funciona.

--- app/src/main/java/mx/arr/androidexam/NewsFeed.kt
+++ app/src/main/java/mx/arr/androidexam/NewsFeed.kt
@@ -1,22 +1,44 @@
 package mx.arr.androidexam
 
+import android.content.Context
 import android.content.Intent
+import android.content.SharedPreferences
 import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_news_feed.*
 
 class NewsFeed : AppCompatActivity(), ClickListener {
 
+    //arreglo con noticias
     private lateinit var arrNoticias: Array<Noticia>
+
+    // sharedPreferences
+    private lateinit var prefs : SharedPreferences
+    private lateinit var editor: SharedPreferences.Editor
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news_feed)
 
+        //Configurar las prefs
+        prefs = getSharedPreferences("Login", Context.MODE_PRIVATE)
+        editor = prefs.edit()
+
         configurarRV()
     }
+
+    //Finalizar sesion
+    fun finalizarSesion(v : View){
+        editor.clear()
+        editor.commit()
+        val intMain = Intent(this, MainActivity::class.java)
+        startActivity(intMain)
+        finish()
+    }
+
 
     private fun configurarRV() {
         val layout = LinearLayoutManager(this)
@@ -31,7 +53,7 @@ class NewsFeed : AppCompatActivity(), ClickListener {
 
     private fun crearArrNoticias(): Array<Noticia> {
         return arrayOf(
-            Noticia("Tren Maya", "Excelsior", 1)
+            Noticia("Tren Maya", "Excelsior", R.drawable.silueta__1_)
         )
     }
 
